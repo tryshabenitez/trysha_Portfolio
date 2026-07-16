48cf79d7e197b04198628888345e155be2cf47fd
tryshabenitez <trysharhoy.benitez@gsfe.tupcavite.edu.ph>
2026-07-16 18:33:13 +0800

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,16 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "main" / "static",
+    BASE_DIR / 'main' / 'static',
 ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "main" / "static",
-]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
